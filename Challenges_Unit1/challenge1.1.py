38218b617830da0ee0248986de0569d73c69ed0a
THEJU23 <144332606+THEJU23@users.noreply.github.com>
2023-09-07 15:15:03 +0530

    Add files via upload

---
+++ Challenges_Unit1/challenge1.1.py
@@ -0,0 +1,9 @@
+#online python compiler (interpreter)to run python online
+#write python 3 code in this online editor and run it
+def fact(n):
+     if n==0 or n==1:
+        return 1
+     else:
+       return n* fact(n)
+print(fact(5))
+
---
+++ Challenges_Unit1/challenge1.2.py
@@ -0,0 +1,7 @@
+# online python compiler (interpreter) or run python online.
+# write python 3 code in this online editor and run it
+year = 2023 
+if(year%4 == 0):
+  print ("Leap year")
+else:
+  print("Not Leap year")
